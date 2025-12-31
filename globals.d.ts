193a329e9ba97e10895659e764daea792ce3164f
Victor Alvarez <victoralvarezsaucedo@gmail.com>
2025-12-31 14:54:40 -0700

    fix build

--- globals.d.ts
+++ globals.d.ts
@@ -1,3 +1,28 @@
 declare module 'pdfmake'
 declare module '@yaireo/tagify/react'
 declare module 'apextree'
+
+declare module "*.ico" {
+    const content: any;
+    export default content;
+}
+declare module "*.png" {
+    const content: any;
+    export default content;
+}
+declare module "*.jpg" {
+    const content: any;
+    export default content;
+}
+declare module "*.jpeg" {
+    const content: any;
+    export default content;
+}
+declare module "*.svg" {
+    const content: any;
+    export default content;
+}
+declare module "*.gif" {
+    const content: any;
+    export default content;
+}
